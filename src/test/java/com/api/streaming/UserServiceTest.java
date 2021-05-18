c54446c46ce0a1cb4d538e0e5ecb71c7388f93f3
DiegoAvmor <diegoavmor@gmail.com>
2021-05-18 14:31:52 -0500

    Adición generación de token para instanciar la sesión

--- src/test/java/com/api/streaming/UserServiceTest.java
+++ src/test/java/com/api/streaming/UserServiceTest.java
@@ -1,53 +1,70 @@
 package com.api.streaming;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import com.api.streaming.exception.AccessDeniedException;
-import com.api.streaming.exception.NotFoundException;
+import com.api.streaming.model.User;
+import com.api.streaming.model.dto.TokenDto;
 import com.api.streaming.model.request.LoginUserRequest;
-import com.api.streaming.repository.RatingRepository;
-import com.api.streaming.repository.RecommendationRepository;
-import com.api.streaming.repository.UserPreferencesTagsRepository;
 import com.api.streaming.repository.UserRepository;
-import com.api.streaming.repository.VideoRepository;
 import com.api.streaming.service.impl.UserServiceImpl;
 import com.api.streaming.util.JwtTokenUtil;
-
+import org.jose4j.jwt.JwtClaims;
+import org.jose4j.jwt.consumer.JwtConsumer;
+import org.jose4j.jwt.consumer.JwtConsumerBuilder;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.event.annotation.BeforeTestMethod;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+import com.api.streaming.util.RolesConstants;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 @SpringBootTest
 public class UserServiceTest {
 
     @Autowired
     UserServiceImpl userServiceImpl;
-
-    @Autowired
-    RatingRepository ratingRepository;
-
-    @Autowired
-    VideoRepository videoRepository;
-
-    @Autowired
-    RecommendationRepository recommendationRepository;
-
-    @Autowired
-    UserPreferencesTagsRepository userPreferencesTagsRepository;
     
     @Autowired
     UserRepository userRepository;
+
+    @Autowired
+    private JwtTokenUtil jwtUtil;
 
     @BeforeEach
     public void setup(){
         LoginUserRequest request = new LoginUserRequest();
         request.setEmail("carlos@outlook.es");
         request.setPassword("12345678");
-        userServiceImpl.loadUser(request);
+        TokenDto token = userServiceImpl.loadUser(request);
+        try {
+            JwtConsumer jwtConsumer = new JwtConsumerBuilder().setSkipSignatureVerification().build();
+            JwtClaims claims = jwtConsumer.processToClaims(token.getToken());
+            Optional<User> usuario = userRepository.findById(Integer.valueOf(claims.getJwtId()));
+
+            if(!usuario.isPresent()){
+                return;
+            }
+
+            User user = usuario.get();
+            boolean isValid = jwtUtil.validateToken(token.getToken(), user);
+            if (!isValid) {
+                return;
+            }
+    
+            //Se añade las autorizaciones/roles
+            Set<SimpleGrantedAuthority> authorities = new HashSet<>();
+            authorities.add(new SimpleGrantedAuthority(RolesConstants.ROLE_AUTHORITY_PREFIX+ user.getRole().getName()));
+            Authentication authentication = new UsernamePasswordAuthenticationToken(user, null, authorities);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        } catch (Exception e) {
+            return;
+        }
     }
 
     @Test
